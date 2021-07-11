ec00c41ed4e61c8c380a3d242e211a73c2588a36
Albert Anugerah Putra Siregar <albertanugerah@hotmail.com>
2021-07-11 10:48:55 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+// notes
 const notes = require('./api/notes');
+const ClientError = require('./exceptions/ClientError');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UserService');
+const UsersValidator = require('./validator/users');
+
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -18,12 +26,61 @@ const init = async () => {
 
   // server.route(routes);
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
+  server.ext('onPreResponse', (request, h) => {
+    // mendapatkan konteks respose dari request
+    const { response } = request;
+
+    if (response instanceof ClientError) {
+      // membuat response baru dari response toolkit sesuai kebutuhan error handling
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+    // Server ERROR!
+    const newResponse = h.response({
+      status: 'error',
+      message: 'Maaf, terjadi kegagalan pada server kami.',
+    });
+    newResponse.code(500);
+
+    // jika bukan ClientError, lanjutkan degan repsonse sebelumnya (tanpa intervensi)
+    return response.continue || response;
+  });
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+    const newResponse = h.response({
+      status: 'error',
+      message: 'Maaf, terjadi kegagalan pada server kami.',
+    });
+    newResponse.code(500);
+    return response.continue || response;
   });
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
--- src/services/postgres/UserService.js
+++ src/services/postgres/UserService.js
@@ -10,19 +10,19 @@ class UsersService {
   }
 
   async addUser({ username, password, fullname }) {
-    // TODO: Verifikasi username, pastikan belum terdaftar.
     await this.verifyNewUsername(username);
 
-    // TODO: Bila verifikasi lolos, maka masukkan user baru ke database.
     const id = `user-${nanoid(16)}`;
     const hashedPassword = await bcrypt.hash(password, 10);
     const query = {
-      text: 'INSERT INTO users VALUES($1,$2,$3,$4)',
+      text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
       values: [id, username, hashedPassword, fullname],
     };
+
     const result = await this._pool.query(query);
-    if (!result.rowCount) {
-      throw new InvariantError('User gagal ditambahkan.');
+
+    if (!result.rows.length) {
+      throw new InvariantError('User gagal ditambahkan');
     }
     return result.rows[0].id;
   }
@@ -34,9 +34,9 @@ class UsersService {
     };
 
     const result = await this._pool.query(query);
-    if (result.rowCount > 0) {
+    if (result.rows.length > 0) {
       throw new InvariantError(
-        'Gagal menambahkan user, Username sudah digunakan.'
+        'Gagal menambahkan user. Username sudah digunakan.'
       );
     }
   }
